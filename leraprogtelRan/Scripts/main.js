4b66a0076e22a6db2f27a407f487df5e41e80efb
valerypeterkova <1404vall@gmail.com>
2018-03-28 09:51:23 +0300

    fonts

---
+++ leraprogtelRan/Scripts/main.js
@@ -0,0 +1,53 @@
+
+$("#right-col-icon").on("click",function () {
+    $("#menu").css({"display":"none"});
+});
+$("#menu-icon-btn").on("click",function () {
+    $("#menu").css({"display":"block"});
+});
+
+var data = [{name: ["CONCERTS", "SHOWS", "MUSICALS"]}];
+for(var i = 0; i < data[0]["name"].length; i++){
+    var elem = $("<li></li>").text(data[0]["name"][i]);
+    $("#drop-select-option").append(elem);
+}
+
+
+
+$('#drop-select').append(data[0]["name"][0]);
+
+
+
+$("#drop-select").on("click", function () {
+    if($('#drop-select-option').css('display') == 'block') {
+        $("#drop-select-option").css({"display": "none"});
+
+    }else{
+        $("#drop-select-option").css({"display": "block"});
+        $('#drop-select-option li').on('click', function () {
+           $('#drop-select').text(this.textContent);
+           $('#drop-select-option').css({'display': "none"});
+        });
+    }
+});
+
+
+$(".img-ev").on("click",function () {
+    $("#second1").css({"display":"block"});
+    $("#second").css({"display": "none"});
+
+});
+$("#logo1").on("click",function () {
+    $("#second1").css({"display":"none"});
+    $("#second").css({"display": "block"});
+});
+
+$("#log-container").on("click",function () {
+    $("#events").css({"display":"none"});
+    $("#registration").css({"display": "block"});
+});
+
+$("#logo1").on("click",function () {
+    $("#events").css({"display":"block"});
+    $("#registration").css({"display": "none"});
+});
